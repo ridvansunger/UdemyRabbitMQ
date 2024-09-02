13802a990197c26cc7fe9d0efcfbab4297153285
ridvan.sunger <rdvansunger@gmail.com>
2024-09-02 21:47:44 +0300

    Complex_Type

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using RabbitMQ.Client;
+using Shared;
 using System.Collections;
 using System.Text;
+using System.Text.Json;
 
 
 
@@ -17,7 +19,6 @@ var channel = connection.CreateModel();
 
 channel.ExchangeDeclare("header-exhange", durable: true, type: ExchangeType.Headers);
 
-Random rnd = new Random();
 
 Dictionary<string, object> headers = new Dictionary<string, object>();
 headers.Add("format", "pdf");
@@ -27,7 +28,18 @@ headers.Add("shape", "a4");
 var properties = channel.CreateBasicProperties();
 properties.Headers = headers;
 properties.Persistent = true;//Mesajları kalıcı hale getirir.
-channel.BasicPublish("header-exhange", string.Empty, properties, Encoding.UTF8.GetBytes("header mesajım"));
+
+var product = new Product
+{
+    Id = 1,
+    Name = "Kalem",
+    Price = 100,
+    Stock = 10
+};
+
+var productJsonString = JsonSerializer.Serialize(product);
+
+channel.BasicPublish("header-exhange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
 
 Console.WriteLine("Mesaj Gönderilmiştir.");
 
--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Shared;
 using System.Reflection.PortableExecutable;
 using System.Text;
+using System.Text.Json;
 
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://hlzbdrdx:IZ7hjd746v3qYvfPDia_l12RouFKPUHP@toad.rmq.cloudamqp.com/hlzbdrdx");//appsetting de tutulur.
@@ -19,13 +21,13 @@ var consumer = new EventingBasicConsumer(channel);
 
 var queueName = channel.QueueDeclare().QueueName;
 
-Dictionary<string,object> headers=new Dictionary<string, object>();
+Dictionary<string, object> headers = new Dictionary<string, object>();
 
 headers.Add("format", "pdf");
 headers.Add("shape", "a4");
 headers.Add("x-match", "any");//all dersek her bir key value çiftinin aynı olması gerekir.
 
-channel.QueueBind(queueName, exchange: "header-exhange",string.Empty,headers);
+channel.QueueBind(queueName, exchange: "header-exhange", string.Empty, headers);
 
 channel.BasicConsume(queueName, false, consumer);
 
@@ -35,10 +37,12 @@ consumer.Received += (object sender, BasicDeliverEventArgs e) =>
 {
     var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
-    Thread.Sleep(1500);
-    Console.WriteLine("Gelen mesaj " + message);
+    Product product = JsonSerializer.Deserialize<Product>(message);
 
- 
+    Thread.Sleep(1500);
+    Console.WriteLine($"Gelen mesaj :{product.Id}-- {product.Name}--{product.Price}--{product.Stock}");
+
+
 
     channel.BasicAck(e.DeliveryTag, false);//ilgili mesajı bildirir.
 };
